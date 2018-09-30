bda6c02f68a4f37c0a0b6cc203b97c279a5c5988
geraldosalazar16 <geraldosalazar16@gmail.com>
2018-09-30 13:15:17 +0200

    Adaptaciones al cotizador

--- imnc/controllers/agenda_prospectos.js
+++ imnc/controllers/agenda_prospectos.js
@@ -6,7 +6,6 @@ $scope.file_url = "ExpedienteArchivos.php";
 var allEvents = true;
 var id_user = sessionStorage.getItem("id_usuario");
 $scope.mostrar_guardar = true;
-//Prueba
 $scope.mostrar_cerrar = true;
 		
 
