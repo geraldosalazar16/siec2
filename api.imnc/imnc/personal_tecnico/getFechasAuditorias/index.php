3a7f2b2bf8b7d8ec4246ba2634c3d3eaba62210a
geraldosalazar16 <geraldosalazar16@gmail.com>
2019-01-22 11:34:49 +0100

    Ajustes agenda personal auditores

--- api.imnc/imnc/personal_tecnico/getFechasAuditorias/index.php
+++ api.imnc/imnc/personal_tecnico/getFechasAuditorias/index.php
@@ -21,14 +21,13 @@ function valida_error_medoo_and_die(){
 		$respuesta['resultado']="error";
 		$respuesta['mensaje']="Error al ejecutar script: " . $database->error()[2];
 		print_r(json_encode($respuesta));
-		$mailerror->send("certificando", getcwd(), $database->error()[2], $database->last_query(), "polo@codeart.mx");
 		die();
 	}
 }
 
 $respuesta=array();
 $id = $_REQUEST["id"];
-
+/*
 $ids_califs = $database->select("PERSONAL_TECNICO_CALIFICACIONES", "ID", ["ID_PERSONAL_TECNICO" => $id]);
 valida_error_medoo_and_die();
 
@@ -52,7 +51,50 @@ for ($i=0; $i < count($datos_auditoria) ; $i++) {
 }
 
 print_r(json_encode($datos_auditoria));
+*/
+$ids_califs = $database->select("PERSONAL_TECNICO_CALIFICACIONES", "ID", ["ID_PERSONAL_TECNICO" => $id]);
+valida_error_medoo_and_die();
 
+$datos_auditoria = $database->select("I_SG_AUDITORIA_GRUPOS", [
+	"ID_SERVICIO_CLIENTE_ETAPA", 
+	"TIPO_AUDITORIA",
+	"CICLO"
+], [
+	"ID_PERSONAL_TECNICO_CALIF" => $ids_califs
+]);
+valida_error_medoo_and_die();
 
+for ($i=0; $i < count($datos_auditoria) ; $i++) { 
+	$id_sce = $datos_auditoria[$i]["ID_SERVICIO_CLIENTE_ETAPA"];
+	$datos_sce = $database->get("SERVICIO_CLIENTE_ETAPA",[
+		"ID_TIPO_SERVICIO",
+		"REFERENCIA"
+	],[
+		"ID" => $id_sce
+	]);
+	$id_tipo_servicio = $datos_sce["ID_TIPO_SERVICIO"];
+	$referencia = $datos_sce["REFERENCIA"];
+	valida_error_medoo_and_die();
+
+	$datos_auditoria[$i]["ID_TIPO_SERVICIO"] = $id_tipo_servicio;
+	$datos_auditoria[$i]["REFERENCIA"] = $referencia;
+	
+	$normas = $database->select("SCE_NORMAS","ID_NORMA",["ID_SCE" => $id_sce]);
+	valida_error_medoo_and_die();
+	$datos_auditoria[$i]["NORMAS"] = $normas;
+
+	$sg_auditoria_grupo_fechas = $database->select("I_SG_AUDITORIA_GRUPO_FECHAS", "FECHA", [
+		"AND" => [
+			"ID_SERVICIO_CLIENTE_ETAPA" => $id_sce,
+			"TIPO_AUDITORIA" => $datos_auditoria[$i]["TIPO_AUDITORIA"],
+			"CICLO" => $datos_auditoria[$i]["CICLO"],
+			"ID_PERSONAL_TECNICO_CALIF" => $ids_califs
+		]
+	]);
+	valida_error_medoo_and_die();
+	$datos_auditoria[$i]["FECHAS_ASIGNADAS"] = $sg_auditoria_grupo_fechas;
+}
+
+print_r(json_encode($datos_auditoria));
 //-------- FIN --------------
 ?>
