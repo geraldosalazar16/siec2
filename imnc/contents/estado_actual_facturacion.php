cc693d9613a59ffd1c3c338ead06f791f82f575c
leo <leonar2zb@hotmail.com>
2019-07-19 01:18:38 -0400

    grafica cant fact x estados y el monto x estados en 2 graf pastel

--- imnc/contents/estado_actual_facturacion.php
+++ imnc/contents/estado_actual_facturacion.php
@@ -14,171 +14,45 @@
 
 				<div class="row x_title">
 					<div class="col-md-6">
-						<h3>Anexo 1 <small>Cantidad de facturas por estado</small></h3>
+						<h3>Estado actual facturaci&oacuten</h3>
 					</div>
 				</div>
 				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'>Reporte Certificados Vigentes {{ano_actual}}</h5>
+					<h4 align='center'>Cantidad de facturas por estado</h4>
 					<div style="width: 100%;">
 						<canvas id="RepCantFactxEstad" height="80"></canvas>
 					</div>
-				</div>
-				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'>Hist&oacutericos Reporte Certificados Vigentes</h5>
-					<div style="width: 100%;">
-						<canvas id="RepCertVigHistChart" height="80"></canvas>
-					</div>
-				</div>
+				</div>							
 
              </div>
 
             <div class="clearfix" style="background-color:white;"></div>
         </div>
     </div>
-		     <br />
-
 	<div class="row">
 		<div class="col-md-12 col-sm-12 col-xs-12" style="overflow: auto;overflow-y: hidden;">
-			<div class="dashboard_graph" id="pruebaDaniel1">
-
-				<div class="row x_title">
-					<div class="col-md-6 ">
-						<h3>Anexo 2 <small>Indicadores Certificación de Sistemas de Gesti&oacute;n</small></h3>
-					</div>
-				</div>
-				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'>Reporte Comparativa de Contrataci&oacuten {{ano_actual}}</h5>
-					<div style="width: 100%;">
-						<canvas id="repCompContrChart" height="80"></canvas>
-					</div>
-				</div>
-				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'>Hist&oacutericos Reporte Comparativa de Contrataci&oacuten </h5>
-					<div style="width: 100%;">
-						<canvas id="repCompContrHistChart" height="80"></canvas>
-					</div>
-				</div>
-
-             </div>
-
-            <div class="clearfix" style="background-color:white;"></div>
-        </div>
-    </div>
-		     <br />		
+		</div>
+	</div>
+	<br />		
 	<div class="row">
 		<div class="col-md-12 col-sm-12 col-xs-12" style="overflow: auto;overflow-y: hidden;">
-			<div class="dashboard_graph" id="pruebaDaniel2">
-
-				<div class="row x_title">
-					<div class="col-md-6">
-						<h3>Anexo 3 <small>Indicadores Certificación de Sistemas de Gesti&oacute;n</small></h3>
-					</div>
-				</div>
+			<div class="dashboard_graph" id="grafica2">				
 				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'>Reporte Mezcla de Portafolio {{ano_actual}}</h5>
+					<h4 align='center'>Monto de facturas por estado</h4>
 					<div style="width: 100%;">
-						<canvas id="repMezclaPortChart" height="80"></canvas>
+						<canvas id="RepMontoFactxEstad" height="80"></canvas>
 					</div>
-				</div>
-				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'>Hist&oacutericos Reporte Mezcla de Portafolio</h5>
-					<div style="width: 100%;">
-						<canvas id="repMezclaPortHistChart" height="80"></canvas>
-					</div>
-				</div>
+				</div>							
 
              </div>
 
             <div class="clearfix" style="background-color:white;"></div>
         </div>
     </div>
-		     <br />	
-	<div class="row">
-		<div class="col-md-12 col-sm-12 col-xs-12" style="overflow: auto;overflow-y: hidden;">
-			<div class="dashboard_graph" id="pruebaDaniel2">
 
-				<div class="row x_title">
-					<div class="col-md-6">
-						<h3>Anexo 4 <small>Indicadores Certificación de Sistemas de Gesti&oacute;n</small></h3>
-					</div>
-				</div>
-				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'>Reporte Certificados emitidos SG {{ano_actual}}</h5>
-					<div style="width: 100%;">
-						<canvas id="repCertEmitSGChart" height="80"></canvas>
-					</div>
-				</div>
-				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'> Hist&oacutericos Reporte Certificados emitidos SG </h5>
-					<div style="width: 100%;">
-						<canvas id="repCertEmitSGHistChart" height="80"></canvas>
-					</div>
-				</div>
-
-             </div>
-
-            <div class="clearfix" style="background-color:white;"></div>
-        </div>
-    </div>
-		     <br />	
-	<div class="row">
-		<div class="col-md-12 col-sm-12 col-xs-12" style="overflow: auto;overflow-y: hidden;">
-			<div class="dashboard_graph" id="pruebaDaniel2">
-
-				<div class="row x_title">
-					<div class="col-md-6">
-						<h3>Anexo 5 <small>Indicadores Certificación de Sistemas de Gesti&oacute;n</small></h3>
-					</div>
-				</div>
-				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'>Reporte Servicios realizados SG {{ano_actual}}</h5>
-					<div style="width: 100%;">
-						<canvas id="repServRealizSGChart" height="80"></canvas>
-					</div>
-				</div>
-				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'> Hist&oacutericos Reporte Servicios realizados SG </h5>
-					<div style="width: 100%;">
-						<canvas id="repServRealizSGHistChart" height="80"></canvas>
-					</div>
-				</div>
-
-             </div>
-
-            <div class="clearfix" style="background-color:white;"></div>
-        </div>
-    </div>
-		     <br />
-	<div class="row">
-		<div class="col-md-12 col-sm-12 col-xs-12" style="overflow: auto;overflow-y: hidden;">
-			<div class="dashboard_graph" id="pruebaDaniel2">
-
-				<div class="row x_title">
-					<div class="col-md-6">
-						<h3>Anexo 6 <small>Indicadores Certificación de Sistemas de Gesti&oacute;n</small></h3>
-					</div>
-				</div>
-				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'>Reporte D&iacuteas auditor SG {{ano_actual}}</h5>
-					<div style="width: 100%;">
-						<canvas id="repDiasAudSGChart" height="80"></canvas>
-					</div>
-				</div>
-				<div class="col-md-12 col-sm-12 col-xs-12 bg-white">
-					<h5 align='center'> Hist&oacutericos Reporte D&iacuteas auditor SG </h5>
-					<div style="width: 100%;">
-						<canvas id="repDiasAudSGHistChart" height="80"></canvas>
-					</div>
-				</div>
-
-             </div>
-
-            <div class="clearfix" style="background-color:white;"></div>
-        </div>
-    </div>
-		     <br />				 
+	<br />				 
 
 
-      </div> 
+    </div> 
 
 </span>
--- imnc/controllers/estado_actual_facturacion.js
+++ imnc/controllers/estado_actual_facturacion.js
@@ -1,62 +1,59 @@
 app.controller('estado_actual_facturacion_controller', ['$scope', '$http', function($scope, $http) {
-    var repCantFactxEstad = document.getElementById("RepCantFactxEstad");
-    console.log("Llamado");
-   /* QUITAR 
-    $scope.prospecto_seguimiento = [];
-    $scope.formData = {};
-    $scope.prospectos = []; */
+	var repCantFactxEstad = document.getElementById("RepCantFactxEstad");
+	var repMontoFactxEstad = document.getElementById("RepMontoFactxEstad");
+
+	var datos=[];
+	var estado=[];
+	var fondo=[];      
 
 // Graficar cantidad de facturas según estado
-$scope.graficaRepCantFactxEstad = function(){
-    console.log("entro");    
+$scope.graficaRepCantFactxEstad = function(){    
 	$.ajax({
 		type:'GET',
 		dataType: 'json',
 		url:global_apiserver+"/facturacion_resumen/GetCountByStatus/",
-		success:function(data){
-            console.log("entro aquí tambiém");
-            console.log(data);
-            datos=[];
-            estado={};
-            data.forEach(function(valor,indice){
-                estado.label=valor.estatus;
-                console.log("estado: ",valor.estatus);
-                estado.data=valor.cantidad;
-                estado.backgroundColor='rgb(Math.round( Math.random( ) * 255 ),Math.round( Math.random( ) * 255 ),Math.round( Math.random( ) * 255 ),0.7)';
-                datos.push(estado);
-            });
+		success:function(data){            
+			colores=["red","blue","lime","black","pink","green","purple","gray","black","orange"];
+			for(var i = 0 ; i < data.length ; i++){				
+				datos[i] = data[i].cantidad;
+				estado[i] = data[i].estatus;
+				fondo[i]=colores[i];//rgba(Math.round( Math.random( ) * 255 ),Math.round( Math.random( ) * 255 ),Math.round( Math.random( ) * 255 ),0.7)';
+			}			
 
 			var mybarChart = new Chart(repCantFactxEstad, {
-				type: 'bar',
+				type: 'pie',
 				data: {
-                    labels: datos.estatus,
-                    datasets: datos
-					/*datasets: [{
-						label: 'AUDITORES EXTERNOS (%)',
-						backgroundColor: 'rgba(255, 0, 0, 0.7)',
-						data: data.Y1,
-					},{
-						label: 'AUDITORES INTERNOS (%)',
-						backgroundColor: 'rgba(0, 0, 255, 0.7)',
-						data: data.Y2,
-					}]*/
-				},
-
-				options: {
-					
-					scales: {
-						xAxes: [{
-							stacked: true
-
-						}],
-						yAxes: [{
-							stacked: true
-						}]	
-					}
-					
+                    labels: estado,
+					datasets: [{data: datos,					  
+					  backgroundColor: fondo}]					
 				}	
-			});
-	        console.log("chart: ",mybarChart.data);
+			});	        
+		}
+	});
+};
+
+// Graficar monto de facturas según estado
+$scope.graficaRepMontoFactxEstad = function(){    
+	$.ajax({
+		type:'GET',
+		dataType: 'json',
+		url:global_apiserver+"/facturacion_resumen/GetAmountByStatus/",
+		success:function(data){            
+			colores=["red","blue","lime","black","pink","green","purple","gray","black","orange"];
+			for(var i = 0 ; i < data.length ; i++){				
+				datos[i] = data[i].amount;
+				estado[i] = data[i].estatus;
+				fondo[i]=colores[i];//rgba(Math.round( Math.random( ) * 255 ),Math.round( Math.random( ) * 255 ),Math.round( Math.random( ) * 255 ),0.7)';
+			}			
+
+			var mybarChart = new Chart(repMontoFactxEstad, {
+				type: 'pie',
+				data: {
+                    labels: estado,
+					datasets: [{data: datos,					  
+					  backgroundColor: fondo}]					
+				}	
+			});	        
 		}
 	});
 };
@@ -71,8 +68,8 @@ $scope.graficaRepCantFactxEstad = function(){
             });
      }*/
 
-    console.log("paso 1");
-    $scope.graficaRepCantFactxEstad();
-    console.log("paso 2");
+	$scope.graficaRepCantFactxEstad();
+	$scope.graficaRepMontoFactxEstad();
+    
 
 }])
